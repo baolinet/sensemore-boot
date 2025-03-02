2a8e4363e4dff58fde63d782369099b80b2d0e5a
baolinet <bao.li.net@hotmail.com>
2025-03-02 12:21:40 +0800

    初始化项目

---
+++ sensemore-api/src/test/java/com/sensemore/api/SensemoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensemore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensemoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sensemore-core/src/test/java/com/sensemore/core/SensemoreCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensemore.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensemoreCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
